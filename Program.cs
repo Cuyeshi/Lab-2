a86ffe0e2499068b504641eb8bf3d611efc7cb13
Cuyeshi <Cuyeshi@DESKTOP-2GTDQ2V>
2023-09-27 21:18:42 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, m;
+            n = Program.ReadDouble();
+            Matrix[,] matrix = new Matrix[n,m];
+        }
+
+        /// <summary> 
+        /// Метод для проверки вводимых данных на корректность. 
+        /// </summary> 
+        /// <returns></returns> 
+        public static double ReadDouble()
+        {
+            string numeral = Console.ReadLine();
+            double value;
+            while (!Double.TryParse(numeral, out value))
+            {
+                Console.WriteLine("\nВведённые данные не подходят. Введите корректное значение: ");
+                numeral = Console.ReadLine();
+            }
+            return value;
+        }
+
+        public static int ReadDouble()
+        {
+            string numeral = Console.ReadLine();
+            int value;
+            while (!Double.TryParse(numeral, out value))
+            {
+                Console.WriteLine("\nВведённые данные не подходят. Введите корректное значение: ");
+                numeral = Console.ReadLine();
+            }
+            return value;
+        }
+    }
+
+    class Matrix
+    {
+
+    }
+
+    
+}
